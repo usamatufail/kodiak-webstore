1c9ff9053cc502ac9f9601756d81db7a134f7717
Muhammad Suleman <sulemananwar97@gmail.com>
2023-01-30 10:16:23 +0500

    style(homepage): upadate the styles for homepage section

--- src/containers/home/location.client.tsx
+++ src/containers/home/location.client.tsx
@@ -4,7 +4,10 @@ export const Location = () => {
       className="mt-[8px] mb-[8px] min-h-[450px] bg-center bg-no-repeat bg-cover text-white flex items-center justify-center relative"
       style={{ backgroundImage: 'url(/images/location/background.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
-      <div className="bg-black w-full h-full absolute z-10 opacity-60"></div>
+      <div
+        className="bg-black w-full
+       h-full absolute z-10 opacity-60"
+      ></div>
       <div className="w-full h-full absolute z-20 flex items-center justify-center">
         <h2 className="text-[62px] tracking-[0.01em] capitalize font-bold md:font-[900]">Location</h2>
       </div>
