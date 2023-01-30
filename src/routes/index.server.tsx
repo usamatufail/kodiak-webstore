175a24e45de36956b585c3e7daf9034ec2b331b7
Muhammad Suleman <sulemananwar97@gmail.com>
2023-01-30 09:55:54 +0500

    style(homepage): make news-letter compoenet responsive

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Why />
       {/* <Products /> */}
       <Knives />
-      {/* <NewsLetter /> */}
+      <NewsLetter />
       {/* <Location />  */}
     </Layout>
   );
