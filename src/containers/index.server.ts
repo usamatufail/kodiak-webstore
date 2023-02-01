9990ee809b1f125e32aa6498bf24ee95c8bde7dc
Muhammad Suleman <sulemananwar97@gmail.com>
2023-02-01 12:11:15 +0500

    feat(hunting-knives): added design with responisveness

---
+++ src/containers/index.server.ts
@@ -0,0 +1,3 @@
+//hunting Knives
+export { Types } from './hunting-knives/types.server';
+export { Benefits } from './hunting-knives/benefits.server';
--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '../components/index.server';
-import { Header, Available, Why, Products, Knives, NewsLetter, Location } from '../containers';
+import { Header, Available, Why, Products, Knives, Location } from '../containers';
+import { NewsLetter } from '../components/index';
 
 export default function Home() {
   return (
