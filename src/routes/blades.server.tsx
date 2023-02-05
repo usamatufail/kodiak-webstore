e6c5f360a625fb19d1340c069b055129f4fa6520
Usama Tufail <miansamusama@gmail.com>
2023-02-05 00:34:23 +0000

    feat(blades): added new text in the page

--- src/routes/blades.server.tsx
+++ src/routes/blades.server.tsx
@@ -6,22 +6,42 @@ import { HandmadeKnives } from '../containers/blades/handmade-knives.client';
 
 const huntingTexts = {
   h1: 'Types of Hunting Knives',
-  h1p: `Hunting knives come in many shapes and sizes, so it is important to familiarize yourself with the different types before making a decision. Fixed blade knives are the most common type used by hunters, as they are sturdy and reliable when it comes to skinning animals or performing other tasks. Folding knives are also popular, as they offer convenience and portability but may not be as strong as fixed blades. Finally, some hunters prefer specialized knives such as gut hooks or caping knives for specific tasks.\n\nWhen shopping for a hunting knife, consider its size, shape, weight, blade length, handle material, and sheath design. The size and shape should be comfortable in your hand so that you can easily grip the knife while you work. The weight should be heavy enough to feel solid but light enough that it won't tire out your arm quickly. The blade should be long enough to perform all necessary tasks without being unwieldy or difficult to maneuver. Handle materials vary greatly; some hunters prefer leather while others opt for synthetic materials like G10 or Micarta which offer improved grip in wet conditions. Finally, make sure the sheath design is secure yet easy to access when needed.`,
+  h1p: `Discover the World of Hunting Knives Embark on an exciting adventure with hunting knives, available in a range of shapes and sizes to suit your needs.
+
+  From the dependable fixed-blade knives to the portable folding knives, and the specialized gut hooks and caping knives, you'll find the right tool for any hunting challenge.
+
+  As you shop for your perfect hunting knife, keep in mind important factors such as size and comfort in the hand, weight for effortless use, a blade length that balances versatility and maneuverability, and handle materials that provide a sturdy grip.
+
+  Whether you prefer leather or modern synthetic materials like G10 or Micarta, make sure your sheath design is both secure and easily accessible. With these considerations, you'll be ready to make your mark in the hunting world.
+  `,
   h2: 'Benefits of Owning a Hunting Knife',
-  h2p: `Owning a quality hunting knife offers several advantages over using regular tools such as kitchen knives or pocket knives. A good hunting knife will stay sharp longer than other types of blades due to its thicker steel construction and higher-quality steel used in its manufacture. It also has superior edge retention which means it will stay sharp longer even after extensive use or wear-and-tear from extended outdoor use. Additionally, these types of blades have specialized features such as gut hooks which make them better suited for field dressing game than traditional pocket knives for kitchen cutlery sets would be able to provide.\n\nAt Kodiak Knife Co , we offer a wide range of hunting knives designed to meet the needs of the modern hunter. From specialized caping knives to versatile fixed blades and folding knives, you can find the perfect tool for whatever job you have in mind. Whether you need a reliable skinning or gutting knife, our selection has something for everyone. Shop today and check out our hunting equipment today!`,
+  h2p: `Unleash your inner adventurer with a hunting knife from Kodiak Knife Co. Our high-quality knives offer numerous benefits over conventional tools, such as kitchen knives or pocket knives.
+
+  With a thicker steel construction and premium steel materials, our hunting knives maintain a sharp edge for longer and boast superior edge retention, even after intense use in the field.
+
+   Featuring specialized features, such as gut hooks, our hunting knives are specifically designed to efficiently dress game, surpassing the capabilities of traditional kitchen cutlery and pocket knives.
+
+  Discover the perfect hunting knife to meet your needs from our diverse range of options. From specialized caping knives to versatile folding and fixed blades, we have everything you need for a successful hunting experience. So, upgrade your equipment and shop with us today for a reliable skinning or gutting knife.
+  `,
   h3: ``,
   h3p: ``,
 };
 
 const handmadeTexts = {
   h1: `Quality Materials`,
-  h1p: `When it comes to knives, quality materials make all the difference. Handmade knives use superior materials such as high-carbon stainless steel which is more durable and longer lasting than regular stainless steel blades. These knives also come with handles made from exotic woods like walnut, rosewood, ebony, and teakwood that are not only beautiful but also comfortable and easy to grip. `,
-  h2: `Expert Craftsmanship`,
-  h2p: `The true beauty of handmade knives lies in their craftsmanship. Each knife is crafted with precision and attention to detail by experienced artisans who understand the importance of creating an ergonomic design and balanced weight distribution in order to ensure maximum comfort and performance. The result is a one-of-a-kind knife with superior strength, resilience, and sharpness that will last for years to come.`,
-  h3: 'Quality Assurance',
-  h3p: `When you purchase a handmade knife, you can be sure that each piece has been inspected by an expert craftsman before it was shipped out. This means that you will get a product that has been tested for accuracy and quality, ensuring that it meets all safety standards before it reaches your hands. Additionally, these knives often come with lifetime warranties so if something does happen you know you are covered.
+  h1p: `Unlock your inner adventurer with knives crafted from premium materials.
 
-        At Kodiak Knife Co, we take great pride in providing handmade knives that offer unparalleled quality and craftsmanship. Whether you are looking for a special gift or the perfect addition to your kitchen, our custom-made knives are sure to please. Visit us online today to view our selection of beautiful, handmade knives!`,
+  Indulge in the elegance of handmade blades, forged from high-carbon stainless steel, a superior alternative to regular stainless steel, renowned for its durability and longevity.
+
+  Enhance your cutting experience with handles carved from exotic woods like walnut, rosewood, ebony, and teakwood, adding both beauty and comfort to every grip.
+  `,
+  h2: `Expert Craftsmanship`,
+  h2p: `Handmade knives offer unique qualities such as precision, attention to detail, and ergonomic design. They are made by experienced artisans to ensure maximum comfort, performance, strength, resilience, and sharpness. Each knife is a one-of-a-kind tool that will last for years.`,
+  h3: 'Quality Assurance',
+  h3p: `Each blade, forged with meticulous precision and honed to perfection, undergoes a rigorous inspection by our master craftsmen to ensure it meets the highest standards of accuracy and safety.
+
+  Not only do these one-of-a-kind knives boast exceptional quality and craftsmanship, but they also come with a lifetime warranty - a testament to our unwavering confidence in their durability.
+  `,
 };
 
 export default function Blades() {
@@ -38,7 +58,13 @@ export default function Blades() {
           <HuntingKnives texts={huntingTexts} />
         </div>
         <div className="mt-[4px] mb-[40px]">
-          <HandmadeKnives texts={handmadeTexts} />
+          <HandmadeKnives
+            texts={handmadeTexts}
+            description={`Gain culinary creativity with the magic of handmade knives. These bespoke kitchen tools offer unmatched craftsmanship, elevating your cooking to new heights.
+
+Indulge in a one-of-a-kind gift for a loved one or treat yourself to the ultimate kitchen accessory. Embrace the beauty of handiwork with handmade knives.
+`}
+          />
         </div>
         <DetailsBanner
           backgroundImg="https://res.cloudinary.com/samtufail726/image/upload/f_auto,q_auto/v1675484446/kodiak/Blades/DSC02473_wdpygj.jpg"
