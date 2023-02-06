d312f31c17300983a4d660a528b02d4f38a0be0c
Usama Tufail <miansamusama@gmail.com>
2023-02-06 12:51:42 +0000

    feat(about-us): social section responisveness added

--- src/containers/about/social.client.tsx
+++ src/containers/about/social.client.tsx
@@ -24,23 +24,24 @@ export const Social = () => {
 
   return (
     <div
-      className="w-full mt-[4px] text-center grid grid-cols-2 items-center justify-start px-[300px] overflow-hidden"
+      className="w-full mt-[4px] text-center grid grid-cols-1 md:grid-cols-2 items-center justify-start px-[20px] md:px-[300px] overflow-hidden"
       ref={ref}
       style={{
         backgroundImage:
           'url(https://res.cloudinary.com/samtufail726/image/upload/b_black,o_40/v1675508797/kodiak/About/DSC02050_1_tptii0.jpg)',
         backgroundPosition: 'center',
-        backgroundSize: '100%',
+        backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         minHeight: '450px',
         overflow: 'hidden',
       }}
     >
-      <motion.div initial="initial" animate={controls} variants={leftVariant} className="flex flex-col items-start">
-        <h2 className="text-white text-[44px] font-bold leading-[1.11] text-left mb-[4px]">
+      <motion.div initial="initial" animate={controls} variants={leftVariant} className="flex flex-col items-center md:items-start">
+        <h2 className="hidden md:block text-white text-[44px] font-bold leading-[1.11] text-left mb-[4px]">
           Join the adventure and stay in the know by following us on social media!
         </h2>
-        <p className="text-white text-[18px] text-left mb-[30px]">
+        <h2 className="md:hidden text-white text-[44px] font-bold leading-[1.11] text-center mb-[4px] pt-[20px]">Follow us on social</h2>
+        <p className="text-white text-[16px] md:text-[18px] text-center md:text-left mb-[30px]">
           Stay ahead of the curve and get exclusive access to updates and content from Kodiak Knife Company, your trusted source for
           high-quality, rugged knives.
         </p>
@@ -50,7 +51,12 @@ export const Social = () => {
           </button>
         </a>
       </motion.div>
-      <motion.div initial="initial" animate={controls} variants={rightVariant} className="flex justify-center">
+      <motion.div
+        initial="initial"
+        animate={controls}
+        variants={rightVariant}
+        className="flex justify-center mt-[40px] pb-[40px] md:mt-[0px] md:pb-[0px]"
+      >
         <a href="https://www.instagram.com/kodiak_knife_co/" target="_blank">
           <img
             src="https://res.cloudinary.com/samtufail726/image/upload/v1675509800/kodiak/About/Group_993_feosju.png"
