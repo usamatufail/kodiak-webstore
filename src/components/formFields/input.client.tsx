06e14e501d3d2d54d4850aaaa6fb1d5c31f18f6c
Suleman Anwar <sulemananwar97@gmail.com>
2023-03-10 22:36:33 +0500

    style: :art: update styles for get in touch page

--- src/components/formFields/input.client.tsx
+++ src/components/formFields/input.client.tsx
@@ -1,9 +1,12 @@
-export const Input = ({ icon = '/svg/contact/name.svg', placeholder = 'Name', className = '' }) => {
+export const Input = ({ icon = "", placeholder = "Name", className = "" }) => {
   return (
     <div className={`relative ${className}`}>
-      <img src={icon} className="absolute top-[50%] -translate-y-[50%] left-[12.5px]" />
+      <img
+        src={icon}
+        className="absolute top-[50%] -translate-y-[50%] left-[12.5px]"
+      />
       <input
-        className="h-[52px] bg-[rgba(255,255,255,0.6)] focus:bg-[rgba(255,255,255,0.8)] rounded-[8px] w-full pl-[54px] placeholder:text-black transition-all"
+        className="h-[52px] bg-[rgba(255,255,255,0.6)] focus:bg-[rgba(255,255,255,0.8)] rounded-[8px] w-full  placeholder:text-black transition-all"
         placeholder={placeholder}
       />
     </div>
