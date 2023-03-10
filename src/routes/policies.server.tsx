c8eb174d996e8e719d0ab5415ee3e6db08d389b3
Suleman Anwar <sulemananwar97@gmail.com>
2023-03-10 20:05:01 +0500

    feat: :sparkles: policies page update

--- src/routes/policies.server.tsx
+++ src/routes/policies.server.tsx
@@ -1,10 +1,10 @@
-import { Layout } from '../components/layout.server';
-import { PoliciesContainer } from '../containers/index';
+import { Layout } from "../components/layout.server";
+import { PoliciesContainer } from "../containers/index";
 
 export default function Policies() {
   return (
     <Layout>
-      <div className="md:w-full md:h-screen pt-[40px] md:pt-[0px] pb-[40px] md:pb-[0px] px-[10px] md:px-[0px] flex items-center justify-center bg-[url(https://res.cloudinary.com/samtufail726/image/upload/q_auto/v1675465743/kodiak/DSC02452_ggxiqy.jpg)] bg-cover">
+      <div className="md:w-full pt-[40px] md:pt-[0px] pb-[40px] md:pb-[0px] px-[10px] md:px-[0px] ">
         <PoliciesContainer />
       </div>
     </Layout>
